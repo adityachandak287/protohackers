d160b7f33a99f0887fea947e30e031dc2a84e0db
Aditya Chandak <adityachandak287@gmail.com>
2024-06-19 01:47:03 +0530

    test: add fuzz tests for smoke-test problem
    Fixed bugs in the test code identified by fuzz testing
    - Writing from client connection was blocking, fixed by moving write to a goroutine
    - Input more than 1024 bytes were not read, fixed by creating read buffer based on input length

--- smoke-test/main_test.go
+++ smoke-test/main_test.go
@@ -2,20 +2,31 @@ package main
 
 import (
 	"bytes"
+	"io"
 	"net"
 	"testing"
 )
 
+var testCases = []string{"", " ", "Hello, World!", "!12345"}
+
 func TestSmokeTest(t *testing.T) {
+	for _, tc := range testCases {
+		t.Run("TestCase", func(t *testing.T) {
+			runSmokeTest(t, []byte(tc))
+		})
+	}
+}
+
+func runSmokeTest(t *testing.T, testInput []byte) {
 	server, client := net.Pipe()
 
 	go handleRequest(server, 1)
 
-	testInput := []byte("Hello, World!")
+	go client.Write(testInput)
 
-	client.Write(testInput)
-	buffer := make([]byte, 1024)
-	nBytes, err := client.Read(buffer)
+	inputLen := len(testInput)
+	buffer := make([]byte, inputLen)
+	nBytes, err := io.ReadFull(client, buffer)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -26,4 +37,15 @@ func TestSmokeTest(t *testing.T) {
 	}
 
 	client.Close()
+}
+
+func FuzzSmokeTest(f *testing.F) {
+	// seed corpus
+	for _, tc := range testCases {
+		f.Add([]byte(tc))
+	}
+
+	f.Fuzz(func(t *testing.T, randomString []byte) {
+		runSmokeTest(t, randomString)
+	})
 }
